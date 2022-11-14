f35ba397fdb9d60f19cd58fd4ee9434f8dbbc7f1
sktsherlock <1033270581@qq.com>
2022-11-14 21:41:35 +0800

    gen_shell_env

---
+++ gen_shell_env.py
@@ -0,0 +1,25 @@
+import os.path as osp
+import sys
+import subprocess as sp
+from pathlib import Path
+
+from utils.settings import *
+from private.exp_settings import *
+
+
+PROJ_DIR = osp.abspath(osp.dirname(__file__))[0]
+
+env_vars = {
+    # PATHS
+    'LP': PROJ_DIR,  # Local Path
+    'PROJ_NAME': PROJ_NAME,  # PROJ Name
+}
+
+server_setting_file = f'{PROJ_DIR}/shell_env.sh'
+with open(server_setting_file, 'w') as f:
+    for var_name, var_val in env_vars.items():
+        f.write(f'export {var_name}="{var_val}"\n')
+
+    for cmd in SV_INIT_CMDS:
+        f.write(f'{cmd}\n')
+    print()
